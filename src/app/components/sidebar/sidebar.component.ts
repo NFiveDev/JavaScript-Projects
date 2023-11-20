baf111d969f8d934f5c897963725b55f33cfd632
NFiveDev <nmhjort7@gmail.com>
2023-11-20 22:25:52 +0100

    active links and symbols work

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import { RouterOutlet } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatDividerModule, RouterOutlet],
+  imports: [CommonModule, MatSidenavModule, MatDividerModule, MatListModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 
-export class SidebarComponent {}
+export class SidebarComponent { }
