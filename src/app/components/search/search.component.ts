9fe4dd61aef28ed03f3202525d2ea25377757e0e
NFiveDev <nmhjort7@gmail.com>
2023-11-21 12:55:09 +0100

    working on search btn

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-search',
