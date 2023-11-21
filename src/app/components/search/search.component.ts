0aec061eb933c6517c6caf100d7a6c4da9e56bd9
NFiveDev <nmhjort7@gmail.com>
2023-11-21 13:23:28 +0100

    search latest

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
