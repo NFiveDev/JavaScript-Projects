c91c10f09fa989586c229697f0e9df9406df9e59
NFiveDev <nmhjort7@gmail.com>
2023-11-21 20:51:34 +0100

    search can detect OS

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-search',
   standalone: true,
@@ -8,7 +9,19 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  
+  isWindows: boolean = true;
   @Input() placeholder = '';
   
+  constructor(@Inject(PLATFORM_ID) private _platformId: Object) {  }
+
+  ngOnInit() {
+    if(isPlatformBrowser(this._platformId)) {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      if (userAgent.includes('mac')) {
+        this.isWindows = false;
+      }
+    }
+  }
 }
