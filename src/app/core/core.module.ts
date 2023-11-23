bf8f327e2df04afc575dac6d0b30d1c9b2184204
NFiveDev <nmhjort7@gmail.com>
2023-11-24 00:25:24 +0100

    color overwrites and home page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
-
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AboutComponent,
-    ContactComponent,
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AboutComponent, ContactComponent, HomeComponent],
+  imports: [CommonModule, MatButtonModule],
 })
-export class CoreModule { }
+export class CoreModule {}
