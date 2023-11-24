c1c43d70261859c14dc95bb2df1d35f25f70eea1
NFiveDev <nmhjort7@gmail.com>
2023-11-24 17:30:32 +0100

    feature section and scss

---
+++ src/app/components/feature-card/feature-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-feature-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feature-card.component.html',
+  styleUrl: './feature-card.component.scss'
+})
+export class FeatureCardComponent {
+
+}
