c453fd3afd9c707f03e73f46f6ee7a2630d85a80
NFiveDev <nmhjort7@gmail.com>
2023-11-29 23:18:38 +0100

    search

--- NextMUI/app/components/searchButton.tsx
+++ NextMUI/app/components/searchButton.tsx
@@ -1,18 +1,21 @@
 import { Typography, styled, Box } from '@mui/material';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import KeyboardCommandKeyIcon from '@mui/icons-material/KeyboardCommandKey';
 
 const StyledSearchButton = styled('button')(() => ({
   display: 'flex',
   justifyContent: 'space-between',
   border: '2px solid',
   borderColor: '#e0e0e0',
-  padding: '5px 10px 5px 10px',
+  padding: '5px 8px 5px 8px',
   borderRadius: '10px',
   marginRight: 'auto',
   marginLeft: 'auto',
   marginTop: '1rem',
   marginBottom: '1rem',
   backgroundColor: 'white',
+  width: '180px',
+  cursor: 'pointer'
 }));
 
 export default function SearchButton() {
@@ -22,11 +25,26 @@ export default function SearchButton() {
 
   return (
     <StyledSearchButton onClick={openSearchModalHandler}>
-      <Box sx={{display: 'flex', gap: '0.1rem'}}>
-        <SearchRoundedIcon sx={{color: '#1976d2'}} />
-        <Typography color={'#607d8b'} fontWeight={600} variant='subtitle2'>Quick search...</Typography>
+      <Box sx={{ display: 'flex', gap: '0.1rem', alignItems: 'center' }}>
+        <SearchRoundedIcon sx={{ color: '#1976d2' }} />
+        <Typography
+          color={'#bdbdbd'}
+          fontWeight={600}
+          variant='subtitle2'
+        >
+          Quick search...
+        </Typography>
       </Box>
-      <div></div>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <KeyboardCommandKeyIcon sx={{ width: 16, color: '#757575' }} />
+        <Typography
+          color={'#757575'}
+          fontWeight={500}
+          variant='subtitle2'
+        >
+          K
+        </Typography>
+      </Box>
     </StyledSearchButton>
   );
 }
---
+++ NextMUI/app/utility/sysHelper.ts
@@ -0,0 +1,20 @@
+
+
+function OsHelper() {
+
+  function isWindows(){
+
+  }
+  
+  function isMacOs(){
+  
+  }
+
+  function getOperationSys() {
+
+  }
+
+  return {
+    GetSys: getOperationSys
+  }
+}
