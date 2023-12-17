aa0b8c3115fa03136f1e9574564bc7b1004146ff
NFiveDev <nmhjort7@gmail.com>
2023-12-17 13:56:50 +0100

    taiil start

---
+++ NextTail/src/components/navbar.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react';
+import { HiBars3, HiBell, HiUser, HiChevronDown } from 'react-icons/hi2';
+
+const DrawerToggler = () => {
+  return (
+    <button className='p-1 text-emerald-400 rounded-md hover:bg-slate-500'>
+      <HiBars3 size={28} />
+    </button>
+  );
+};
+
+const Notification = () => {
+  return (
+    <div className='text-emerald-400 p-1 rounded-full hover:bg-slate-500 flex'>
+      <HiBell size={20} />
+    </div>
+  );
+};
+
+const UserQuickActions = () => {
+  return (
+    <div className='text-emerald-400 p-1 rounded-full hover:bg-slate-500 flex items-center'>
+      <div className='inline-block'>
+      <HiUser size={20}/>
+      </div>
+      <div className='inline-block'>
+      <HiChevronDown/>
+      </div>
+    </div>
+  )
+}
+
+const NavBar: FC = () => {
+  return (
+    <div className='bg-slate-600 py-4 px-4'>
+      <div className='max-w-7xl m-auto'>
+        <div className='flex justify-between'>
+          <div>
+            <DrawerToggler />
+          </div>
+          <div className='my-auto flex flex-row gap-2'>
+            <Notification />
+            <UserQuickActions />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
