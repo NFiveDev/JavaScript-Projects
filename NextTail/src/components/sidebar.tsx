51b91e73e1e94b64bbf0b8e9bd8bdb76cccf5ff3
NFiveDev <nmhjort7@gmail.com>
2023-12-17 22:52:48 +0100

    sidebar removed const

--- NextTail/src/components/sidebar.tsx
+++ NextTail/src/components/sidebar.tsx
@@ -5,7 +5,6 @@ import useLayout from "@/hooks/useLayout";
 
 function Sidebar() {
     const {layoutState} = useLayout();
-    const drawerWidth = 250;
 
     return (
         <div className={`fixed md:static bg-slate-600 top-0 bottom-0 left-0 ease-in-out duration-500 w-56 transition-all ${layoutState.sidebarOpen ? 'ms-[-250px]' : 'ms-0'}`}>
