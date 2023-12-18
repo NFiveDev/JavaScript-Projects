f083eefad2891463c45c7e3f72d02bea0d640f1c
NFiveDev <nmhjort7@gmail.com>
2023-12-18 23:50:08 +0100

    working on side

---
+++ NextTail/src/components/divider.tsx
@@ -0,0 +1,3 @@
+export function LineDivier() {
+  return <hr className="my-3 w-11/12 mx-auto border-slate-600"/>
+}
--- NextTail/src/components/sidebar.tsx
+++ NextTail/src/components/sidebar.tsx
@@ -3,7 +3,9 @@
 import { LayoutActionType } from '@/context/layoutProvider';
 import useLayout from '@/hooks/useLayout';
 import Image from 'next/image';
-import { HiChevronLeft } from 'react-icons/hi2';
+import { HiChevronLeft, HiAcademicCap } from 'react-icons/hi2';
+import Link from 'next/link';
+import { LineDivier } from './divider';
 
 function NavLogo() {
   return (
@@ -37,18 +39,86 @@ function CloseSidebarToggle() {
   );
 }
 
+function NavItem(props: { icon: string, label: string, href: string }) {
+  const iconSize = 27;
+
+  const iconResolver = (iconName: string) => {
+    if (iconName === 'course') return <HiAcademicCap size={iconSize} />
+    return <></>
+  }
+
+  return (
+    <Link href={props.href} className='flex flex-row gap-x-5 py-3 text-center px-3'>
+      <span className='text-emerald-600'>
+        {
+          iconResolver(props.icon)
+        }
+      </span>
+      <span className='text-slate-500 font-base text-lg'>
+        {props.label}
+      </span>
+    </Link>
+  )
+}
+
+function NavList() {
+
+  const links = [
+    {
+      href: '/course',
+      label: 'Courses',
+      icon: 'course'
+    },
+    {
+      href: '/course',
+      label: 'Courses',
+      icon: 'course'
+    },
+    {
+      href: '/course',
+      label: 'Courses',
+      icon: 'course'
+    },
+    {
+      href: '/course',
+      label: 'Courses',
+      icon: 'course'
+    },
+    {
+      href: '/course',
+      label: 'Courses',
+      icon: 'course'
+    }
+  ]
+
+  return (
+    <div>
+      <ul>
+        {
+          links.map(function(link, index){
+            return <li key={index + ' ' + link.label} className=' hover:bg-slate-700'><NavItem href={link.href} label={link.label} icon={link.icon}/></li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
 function Sidebar() {
   const { layoutState } = useLayout();
 
   return (
     <div
-      className={`fixed md:static bg-slate-800 top-0 bottom-0 left-0 ease-in-out duration-500 w-[250px] transition-all ${
-        layoutState.sidebarOpen ? 'ms-0' : 'ms-[-250px]'
-      }`}
+      className={`fixed md:static bg-slate-800 top-0 bottom-0 left-0 ease-in-out duration-500 w-[250px] transition-all ${layoutState.sidebarOpen ? 'ms-0' : 'ms-[-250px]'
+        }`}
     >
       <div className='flex flex-row'>
         <NavLogo />
         <CloseSidebarToggle />
+      </div>
+      <LineDivier />
+      <div>
+        <NavList/>
       </div>
     </div>
   );
