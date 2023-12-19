1d39cb0b8ae4b4b20a0711ca4465846bd8f25e9c
NFiveDev <nmhjort7@gmail.com>
2023-12-19 20:21:01 +0100

    macbook cmd

--- NextTail/src/components/buttons/quicksearch.tsx
+++ NextTail/src/components/buttons/quicksearch.tsx
@@ -2,16 +2,20 @@
 
 import { useEffect, useState } from 'react'
 import {HiMagnifyingGlass} from 'react-icons/hi2'
-
+import {FiCommand} from 'react-icons/fi'
 
 export function OsSpecificCommandText() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState<string | JSX.Element>('null');
   const userAgent = window.navigator.userAgent.toLowerCase();
 
   useEffect(() => {
     if(userAgent.indexOf('windows') !== -1){
       setText('ctrl k')
     }
+    if(userAgent.indexOf('intel mac os') !== -1){
+      setText(<div className='flex items-center'><FiCommand /><span>+k</span></div>)
+    }
+    debugger
   }, [userAgent])
 
   return (
@@ -23,7 +27,7 @@ export function OsSpecificCommandText() {
 
 export function QuickSearch() {
   return (
-    <button className="bg-slate-800 px-2 py-1 flex w-48 rounded-md text-center items-center">
+    <button className="bg-slate-800 px-2 py-1 flex w-52 rounded-md text-center items-center">
       <HiMagnifyingGlass size={18} className='inline-block font-medium text-blue-400' />
       <span className="text-sm font-semibold text-slate-500 ms-2">Quick search...</span>
       <span className='flex-grow'/>
